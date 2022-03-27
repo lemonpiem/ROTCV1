2cb98141a8a4e979911c9e35655923e6613c38f6
lemonpiem <97409825+lemonpiem@users.noreply.github.com>
2022-03-26 22:54:31 -0300

    postprocessing listo

--- Assets/Scripts/AddHealth.cs
+++ Assets/Scripts/AddHealth.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class AddHealth : MonoBehaviour
 {
-
+    [SerializeField] private AudioSource soundPlayer;
     private int addHealth;
 
     private void Awake()
     {
         addHealth = 20;
+
     }
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.name == "Player Cirilla")
         {
+            soundPlayer.Play();
             other.gameObject.GetComponent<CiriHealth>().HealPlayer(addHealth);
             int currentHealth = other.gameObject.GetComponent<CiriHealth>().info.currentHealth;
-
             
+
             Destroy(gameObject);
         }
     }
